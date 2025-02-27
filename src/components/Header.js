8944e4bbce1ff8db59ba9e9e7b57ac91cee06233
BanuDanabakiyam <banupriya@quasarinsightlabs.in>
2025-02-27 09:44:22 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -31,64 +31,59 @@ const Header = () => {
 
 	return (
 		<>
-			<div className="container">
-				<header className="header">
-					<nav className="logo-nav-container">
-						<div className="logo">
-							<Link to="/" aria-label="Go to Home Page">
-								<img src="/assets/logo.png" alt="Study Logo" loading="lazy" />
-							</Link>
-						</div>
-					</nav>
-					<div
-						className="menu-icon"
-						onClick={() => setMenuOpen(!menuOpen)}
-						aria-label="Toggle navigation menu"
-					>
-						<img
-							className="img-fluid"
-							src="/assets/hamburger.png"
-							alt="Navigation Menu"
-							width="22"
-							height="22"
-						/>
+			<header className="header container">
+				<nav className="logo-nav-container">
+					<div className="logo">
+						<Link to="/" aria-label="Go to Home Page">
+							<img src="/assets/logo.png" alt="Study Logo" loading="lazy" />
+						</Link>
 					</div>
+				</nav>
+				<div
+					className="menu-icon"
+					onClick={() => setMenuOpen(!menuOpen)}
+					aria-label="Toggle navigation menu"
+				>
+					<img
+						className="img-fluid"
+						src="/assets/hamburger.png"
+						alt="Navigation Menu"
+						width="22"
+						height="22"
+					/>
+				</div>
 
-					<ul
-						className={`nav-links ${menuOpen ? "open" : "close"}`}
-						role="menu"
-					>
-						{navLinks.map((link, index) => (
-							<li
-								key={index}
-								className={defaultActiveIndex === index ? "active" : ""}
-								role="menuitem"
-							>
-								<Link
-									to={link.to}
-									style={{ textDecoration: "none", color: "inherit" }}
-									onClick={handleLinkClick}
-									aria-label={`Go to ${link.name}`}
-								>
-									{link.name}
-								</Link>
-								{activeIndex === index && (
-									<span className="arrow">{"\u276F"}</span>
-								)}
-							</li>
-						))}
-					</ul>
-					<div className="get-purchase-container">
-						<button
-							className="get-purchase"
-							role="button"
-							aria-label="Purchase Now"
+				<ul className={`nav-links ${menuOpen ? "open" : "close"}`} role="menu">
+					{navLinks.map((link, index) => (
+						<li
+							key={index}
+							className={defaultActiveIndex === index ? "active" : ""}
+							role="menuitem"
 						>
-							Purchase Now
-						</button>
-					</div>
-				</header>
-			</div>
+							<Link
+								to={link.to}
+								style={{ textDecoration: "none", color: "inherit" }}
+								onClick={handleLinkClick}
+								aria-label={`Go to ${link.name}`}
+							>
+								{link.name}
+							</Link>
+							{activeIndex === index && (
+								<span className="arrow">{"\u276F"}</span>
+							)}
+						</li>
+					))}
+				</ul>
+				<div className="get-purchase-container">
+					<button
+						className="get-purchase"
+						role="button"
+						aria-label="Purchase Now"
+					>
+						Purchase Now
+					</button>
+				</div>
+			</header>
 		</>
 	);
 };
