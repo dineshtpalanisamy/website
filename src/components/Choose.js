f27aa61189a311da7f344a9dc19ecb18f52a35db
BanuDanabakiyam <banupriya@quasarinsightlabs.in>
2025-02-27 12:58:35 +0530

    commit

--- src/components/Choose.js
+++ src/components/Choose.js
@@ -42,8 +42,66 @@ const Choose = () => {
 								<div className="list-tag">& More</div>
 							</div>
 						</div>
-						<div className="col card-two"></div>
-						<div className="col card-three"></div>
+						<div className="col card-two">
+							<div>
+								<h1>FOR ONLINE COURSE</h1>
+								<h2>
+									Create Your Online
+									<br />
+									Courses Website.
+								</h2>
+							</div>
+							<div>
+								<img
+									src="/assets/02.png"
+									alt="Create your own websites"
+									loading="lazy"
+									style={{
+										width: "100%",
+										height: "100%",
+										objectFit: "contain",
+									}}
+								></img>
+							</div>
+							<div className="list-item">
+								<div className="list-tag">Individual instructor</div>
+								<div className="list-tag">Multiple instructor</div>
+								<div className="list-tag">Market place</div>
+								<div className="list-tag">Single course</div>
+								<div className="list-tag">Like Udemy</div>
+								<div className="list-tag">& More</div>
+							</div>
+						</div>
+						<div className="col card-three">
+							<div>
+								<h1>FOR ONLINE COURSE</h1>
+								<h2>
+									Create Your Online
+									<br />
+									Courses Website.
+								</h2>
+							</div>
+							<div>
+								<img
+									src="/assets/02.png"
+									alt="Create your own websites"
+									loading="lazy"
+									style={{
+										width: "100%",
+										height: "100%",
+										objectFit: "contain",
+									}}
+								></img>
+							</div>
+							<div className="list-item">
+								<div className="list-tag">Individual instructor</div>
+								<div className="list-tag">Multiple instructor</div>
+								<div className="list-tag">Market place</div>
+								<div className="list-tag">Single course</div>
+								<div className="list-tag">Like Udemy</div>
+								<div className="list-tag">& More</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
