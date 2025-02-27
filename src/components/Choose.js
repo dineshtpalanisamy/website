28e2e4a3f597caaf535bf63a6e80126e5e91d23a
BanuDanabakiyam <banupriya@quasarinsightlabs.in>
2025-02-27 11:57:42 +0530

    commit

---
+++ src/components/Choose.js
@@ -0,0 +1,18 @@
+import react from "react";
+import "../style/Choose.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // Include Bootstrap for styling
+
+const Choose = () => {
+	return (
+		<>
+			<div className="">
+				<div className="container choose">
+					<h1 className="all">ALL IN ONE THERE</h1>
+					<h2 className="why">Why Choose Histudy.</h2>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Choose;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Headers from "./Header";
 import Hero from "./Hero";
+import Choose from "./Choose.js";
 
 const Home = () => {
 	return (
 		<>
 			<Headers />
 			<Hero />
+			<Choose />
 		</>
 	);
 };
